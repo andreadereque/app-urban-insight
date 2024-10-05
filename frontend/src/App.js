fddbb82cdab4fa2a8ae841c5a51d5a1089408ad1
andreadereque <aerdna.4422@gmail.com>
2024-10-05 13:26:08 +0200

    clean 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RestaurantList from './components/restaurants/RestaurantList';  // Restaurant map component
-import NationalityAnalysisDashboard from './components/random/NationalityAnalysisDashboard';  // New dashboard component
 import NavigationBar from './components/general/Navbar';  // Navbar
 import InteractiveMaps from './components/demographic_map/InteractiveMaps';
+import Idealista from './components/random/NationalityAnalysis';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <div className="container mt-4">
           <Routes>
             <Route path="/" element={<RestaurantList />} />  {/* Default route */}
-            <Route path="/nationality-analysis" element={<NationalityAnalysisDashboard />} />  {/* Dashboard route */}
+            <Route path="/idealista" element={<Idealista />} />  {/* Dashboard route */}
             <Route path="/interactive-maps" element={<InteractiveMaps />} />  {/* Dashboard route */}
 
           </Routes>
